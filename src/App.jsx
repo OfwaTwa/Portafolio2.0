611b62228f884d061ac1c18c0dec6772f77fa721
Ofwa Twa <ofwatw@gmail.com>
2023-01-05 03:41:40 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from "./componentes/header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style/header.css"
+
+const Header = (props) => {
+    return(
+        <div className="">
+            <header>
+                <nav className="nav container" >
+                    <a href="" >sdafsa</a>
+
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
