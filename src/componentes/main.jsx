8a54758ce2bd00b2370cb20fcf198501ef20ba5f
Ofwa Twa <ofwatw@gmail.com>
2023-01-19 02:18:41 -0500

    Creación de skills

--- src/componentes/main.jsx
+++ src/componentes/main.jsx
@@ -8,7 +8,11 @@ import {
   UilMessage,
   UilMouseAlt,
   UilArrowDown,
-  UilImport
+  UilImport,
+  UilBracketsCurly,
+  UilAngleDown,
+  UilServer,
+  UilSwatchbook
 } from "@iconscout/react-unicons";
 import download from "../archivos/Curriculum.pdf";
 import image from "../imagenes/power.webp";
@@ -102,6 +106,168 @@ const Main = (props) => {
             </div>
           </div>          
         </section>
+
+        {/* skills */}
+        <section className="skills section" id="skills">
+          <h2 className="section__title">Skills</h2>
+          <span className="section__subtitle">My technical level</span>
+
+          <div className="skills__container container grid">
+            <div>
+              {/* Skills 1 */}
+              <div className="skills__content">
+                <div className="skills__header">
+                  <UilBracketsCurly className="skills__icon"/>
+                  <div>
+                    <h1 className="skills__titles">Frontend developer</h1>
+                    <span className="skills__subtitle">More than 4 years</span>
+                  </div>
+                  <UilAngleDown className="skills__arrow"/>
+                </div>
+
+                <div className="skills__list grid">
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">HTML</h3>
+                      <span className="skills__number">90%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__html"></span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">CSS</h3>
+                      <span className="skills__number">40%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__css"></span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">JavaScript</h3>
+                      <span className="skills__number">30%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__js"></span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">React</h3>
+                      <span className="skills__number">20%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__react"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* skills 2 */}
+              <div className="skills__content">
+                <div className="skills__header">
+                  <UilServer className="skills__icon"/>
+                  <div>
+                    <h1 className="skills__titles">Frontend developer</h1>
+                    <span className="skills__subtitle">More than 4 years</span>
+                  </div>
+                  <UilAngleDown className="skills__arrow"/>
+                </div>
+
+                <div className="skills__list grid">
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">PHP</h3>
+                      <span className="skills__number">80%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__php"></span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">Node Js</h3>
+                      <span className="skills__number">60%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__node"></span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">  
+                    <div className="skills__titles">
+                      <h3 className="skills__name">Firebase</h3>
+                      <span className="skills__number">80%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__firebase"></span>
+                    </div>  
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">Python</h3>
+                      <span className="skills__number">85%</span>
+                    </div>  
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__python"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* skills 3 */}
+              <div className="skills__content">
+                <div className="skills__header">
+                  <UilSwatchbook className="skills__icon"/>
+                  <div>
+                    <h1 className="skills__titles">Designer</h1>
+                    <span className="skil ls__subtitle">More than 3 years</span>
+                  </div>
+                  <UilAngleDown className="skills__arrow"/>
+                </div>
+
+                <div className="skills__list grid">
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">Figma</h3>
+                      <span className="skills__number">70%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__figma"></span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">Sketch</h3>
+                      <span className="skills__number">54%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__sketch"></span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className="skills__name">Photoshop</h3>
+                      <span className="skills__number">32%</span>
+                    </div>
+                    <div className="skills__bar">
+                      <span className="skills__percentage skills__photoshop"></span>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
       </main>
     </>
   );
