61d454a8d55373399fb111f2b2f5a7d2dacfada2
Ofwa Twa <ofwatw@gmail.com>
2023-02-05 17:27:10 -0500

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from "./componentes/Header"
 import Main from './componentes/Main'
+import { Footer } from './componentes/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Footer/>
     </div>
   )
 };
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = (props) => {
+    return (
+        <footer className="footer">
+            <div className="footer__bg">
+                
+            </div>
+        </footer>
+    )
+}
