441a01aa383f43f99df51343571ec722fb742d82
Ofwa Twa <ofwatw@gmail.com>
2023-02-05 17:16:19 -0500

    Contact me finish

--- src/componentes/main.jsx
+++ src/componentes/main.jsx
@@ -27,6 +27,9 @@ import {
   UilCheckCircle,
   UilArrow,
   UilPen,
+  UilPhone,
+  UilEnvelopeHeart,
+  UilMapMarker
 } from "@iconscout/react-unicons";
 //imagenes del portafolio
 import portfolio1 from "../imagenes/portfolio1.png";
@@ -867,6 +870,72 @@ const Main = (props) => {
           </div>
         </section>
         
+        {/* Contact Me */}
+        <section className="contact section" id="contact">
+          <h2 className="section__title">Contact Me</h2>
+          <span className="section__subtitle">Get in touch</span>
+
+          <div className="contact__container container grid">
+            <div className="">
+              <div className="contact__information">
+                <UilPhone className="contact__icon" size="2rem" />
+
+                <div>
+                  <h3 className="contact__title">Call Me</h3>
+                  <span className="contact__subtitle">(+57) 301 353 3775</span>
+                </div>
+              </div>
+              
+              <div className="contact__information">
+                <UilEnvelopeHeart className="contact__icon" size="2rem" />
+
+                <div>
+                  <h3 className="contact__title">Email</h3>
+                  <span className="contact__subtitle">ofwatwa@gmail.com</span>
+                </div>
+              </div>
+
+              <div className="contact__information">
+                <UilMapMarker className="contact__icon" size="2rem" />
+
+                <div>
+                  <h3 className="contact__title">Location</h3>
+                  <span className="contact__subtitle">Colombia - Nariño</span>
+                </div>
+              </div>
+            </div>
+
+            <form action="" className="contact__form grid">
+              <div className="contact__inputs grid">
+                <div className="contact__content">
+                  <label htmlFor="" className="contact__label">Name</label>
+                  <input type="text" className="contact__input" />
+                </div>
+
+                <div className="contact__content">
+                  <label htmlFor="" className="contact__label">Email</label>
+                  <input type="email" className="contact__input" />
+                </div>
+              </div>
+              <div className="contact__content">
+                <label htmlFor="" className="contact__label">Project</label>
+                <input type="text" className="contact__input" />
+              </div>
+
+              <div className="contact__content">
+                <label htmlFor="" className="contact__label">Message</label>
+                <textarea name="" id="" cols="0" rows="7" className="contact__input"></textarea>
+              </div>
+
+              <div>
+                <a href="" className="button button--flex">
+                  Send Message
+                  <UilMessage className="button__icon"/>
+                </a>
+              </div>
+            </form>
+          </div>
+        </section>
       </main>
     </>
   );
