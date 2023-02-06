0611857bb6702263295fcc4ad4ae9e8113659090
Ofwa Twa <ofwatw@gmail.com>
2023-02-06 02:12:57 -0500

    Scroll finish

--- src/componentes/Scroll.jsx
+++ src/componentes/Scroll.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 //icon
-import { UilLocationArrowAlt } from '@iconscout/react-unicons'
+import { UilAngleUp } from '@iconscout/react-unicons'
 //Style
 import "../index.css";
 
 export const Scroll = (props) => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      setScrollPosition(position);
+    };
+  
+    useEffect(() => {
+      window.addEventListener("scroll", handleScroll, { passive: true });
+  
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+      
+    }, []);
     return(
         <div>
-            <a href="#" className='scrollup' id='scroll-up'>
-                <UilLocationArrowAlt/>
+            <a href="#" className={`scrollup ${scrollPosition >= 558 ? "show-scroll" : null }`} id='scroll-up'>
+                <UilAngleUp size="1.5rem" className="scrollup__icon"/>
             </a>
         </div>
     )
