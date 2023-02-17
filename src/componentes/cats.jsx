9b1bbc6a3122cc6c1b75a09d28bb648aa1ed212d
Ofwa Twa <ofwatw@gmail.com>
2023-02-17 13:59:15 -0500

    add Umi to the cats

--- src/componentes/cats.jsx
+++ src/componentes/cats.jsx
@@ -1,12 +1,27 @@
-import iconCat from "../imagenes/cats/catIcon.png";
-import "../style/cats.css"
+import { useState } from "react";
 
+import iconCat from "../imagenes/cats/catIcon.png";
+import imageUmi from "../imagenes/cats/Umi.jpg";
+
+import "../style/cats.css"
 const Cats = (props) => {
+
+  const [open, setOpen] = useState(false);
+
+  const onClick = (props) => {
+    if(open){
+      setOpen(false);
+    } else{
+      setOpen(true);
+    }
+  }
+
   return (
     <>
       <div className="image__container">
-        <img src={iconCat} alt=" " className="image__icon"/>
+        <img src={iconCat} alt=" " className="image__icon" onClick={()=>{onClick()}}/>
       </div>
+      <img src={imageUmi} className={`image__Umi ${open ? "image__Umi-open" : null}`} />
     </>
   );
 };
