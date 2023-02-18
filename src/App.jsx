e15e55a15c242d00669399522c43ff3c8d9fe010
Ofwa Twa <ofwatw@gmail.com>
2023-02-18 10:39:50 -0500

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import Header from "./componentes/Header";
-import Main from "./componentes/Main";
-import { Footer } from "./componentes/Footer";
-import { Scroll } from "./componentes/Scroll";
+import Header from "./componentes/Header.jsx";
+import Main from "./componentes/Main.jsx";
+import { Footer } from "./componentes/Footer.jsx";
+import { Scroll } from "./componentes/Scroll.jsx";
 import Cats from "./componentes/cats";
 
 function App() {
