5859d7a6cd99ff9ed15684c5d6bf405813ea7d5d
Ofwa Twa <ofwatw@gmail.com>
2023-05-01 23:26:00 -0500

    Portfolio_v3

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { Cabecera } from "./componentes/Cabecera.jsx";
 import { Home } from "./componentes/Home.jsx";
 import { Footer } from "./componentes/Footer.jsx";
 import { Scroll } from "./componentes/Scroll.jsx";
-import Cats from "./componentes/cats";
-import ContactMe from "./componentes/ContactMe";
+import Cats from "./componentes/Cats";
 import About from "./componentes/About";
 import Skills from "./componentes/Skills";
 import Qualification from "./componentes/Qualification";
 import Services from "./componentes/Servicies";
 import Portfolio from "./componentes/Portfolio";
 import ProjectInMind from "./componentes/ProjectInMind";
+import ContactMe from "./componentes/ContactMe";
 
 function App() {
   return (
--- src/componentes/ContactMe.jsx
+++ src/componentes/ContactMe.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+//email.js
+import emailjs from "emailjs-com";
 //Style
 import "../index.css";
 //Icon
@@ -8,19 +10,15 @@ import {
   UilEnvelopeHeart,
   UilMapMarker,
   UilMessage,
-  UilExclamationOctagon,
-  UilFolderHeart,
 } from "@iconscout/react-unicons";
 //import controler email
 import Joi from "joi";
 
 const ContactMe = () => {
   //Joi
-  const [action, setAction] = useState("javascript:void(0);");
   const [formValues, setFormValues] = useState({
     name: "",
     email: "",
-    project: "",
     message: "",
   });
   const nameValidation = Joi.object({
@@ -30,11 +28,6 @@ const ContactMe = () => {
   });
   const emailValidation = Joi.object({
     email: Joi.string().min(1).message({
-      "string.min": "This field is required",
-    }),
-  });
-  const projectValidation = Joi.object({
-    project: Joi.string().min(1).message({
       "string.min": "This field is required",
     }),
   });
@@ -50,9 +43,6 @@ const ContactMe = () => {
     email: Joi.string().min(1).messages({
       "string.min": "This field is required",
     }),
-    project: Joi.string().min(1).messages({
-      "string.min": "This field is required",
-    }),
     message: Joi.string().min(1).messages({
       "string.min": "This field is required",
     }),
@@ -60,7 +50,6 @@ const ContactMe = () => {
   const [formErrors, setFormErrors] = useState({
     name: "",
     email: "",
-    project: "",
     message: "",
   });
 
@@ -82,15 +71,6 @@ const ContactMe = () => {
     });
   };
 
-  const handleChangeProject = (e) => {
-    const { error } = projectValidation.validate({ project: e.target.value });
-    setFormValues({ ...formValues, project: e.target.value });
-    setFormErrors({
-      ...formErrors,
-      project: error ? error.details[0].message : "",
-    });
-  };
-
   const handleChangeMessage = (e) => {
     const { error } = messageValidation.validate({ message: e.target.value });
     setFormValues({ ...formValues, message: e.target.value });
@@ -100,14 +80,31 @@ const ContactMe = () => {
     });
   };
 
-  useEffect(() => {
+  const sendEmail = (e) => {
+    e.preventDefault();
     const { error: errorGeneral } = formValidation.validate(formValues);
     if (errorGeneral) {
-      setAction("javascript:void(0);");
+      console.log("error");
     } else {
-      setAction("https://formsubmit.co/941f04455a7a3cc513cdda6e36c6db47");
+      emailjs
+        .send(
+          "service_xcixd08",
+          "template_qr13e6p",
+          {
+            from_name: formValues.name,
+            from_email: formValues.email,
+            message: formValues.message,
+          },
+          "DbwsbP5QqqRtkCVAu"
+        )
+        .then((response) => {
+          console.log("Correo enviado", response);
+        })
+        .catch((error) => {
+          console.error("Error al enviar el correo", error);
+        });
     }
-  }, [formValues]);
+  };
 
   return (
     <main className="main">
@@ -146,48 +143,34 @@ const ContactMe = () => {
             </div>
           </div>
 
-          <form action={action} method="POST" className="contact__form grid">
+          <form onSubmit={sendEmail} className="contact__form grid">
             <div className="contact__inputs grid">
               <div className="contact__content">
                 <label htmlFor="" className="contact__label">
                   Name
                 </label>
                 <input
-                  name="name"
                   value={formValues.name}
                   onChange={handleChangeName}
+                  name="name"
                   type="text"
                   className="contact__input"
                 />
                 {formErrors.name || <h2> {formErrors.name} </h2>}
               </div>
-
               <div className="contact__content">
                 <label htmlFor="" className="contact__label">
                   Email
                 </label>
                 <input
-                  name="email"
                   value={formValues.email}
                   onChange={handleChangeEmail}
+                  name="email"
                   type="email"
                   className="contact__input"
                 />
-                {formErrors.email || <h2> {formErrors.email} </h2>}
+              {formErrors.email || <h2> {formErrors.email} </h2>}
               </div>
-            </div>
-            <div className="contact__content">
-              <label htmlFor="" className="contact__label">
-                Project
-              </label>
-              <input
-                name="project"
-                value={formValues.project}
-                onChange={handleChangeProject}
-                type="text"
-                className="contact__input"
-              />
-              {formErrors.project || <h2> {formErrors.project} </h2>}
             </div>
 
             <div className="contact__content">
@@ -210,20 +193,9 @@ const ContactMe = () => {
                 className="button--send button button--flex"
                 type="submit"
               >
-                Send Message{" "}
-                {action === "javascript:void(0);" ? (
-                  <UilExclamationOctagon className="button__icon" />
-                ) : (
-                  <UilMessage className="button__icon" />
-                )}
+                Send Message <UilMessage className="button__icon" />
               </button>
             </div>
-            <input
-              type="hidden"
-              name="_next"
-              value="https://portfolio1-ot.netlify.app/#contact"
-            />
-            <input type="hidden" name="_captcha" value="false" />
           </form>
         </div>
       </section>
