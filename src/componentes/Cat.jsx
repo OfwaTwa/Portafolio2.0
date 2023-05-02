63bbd6331ef3382610a49577e63bb0c09ea781aa
Ofwa Twa <ofwatw@gmail.com>
2023-05-02 16:22:13 -0500

    Portfolio_v3.2

--- src/componentes/Cat.jsx
+++ src/componentes/Cat.jsx
@@ -15,7 +15,7 @@ const Cat = () => {
   return (
     <div>
       <div className="image__container">
-        <span className="image__container"> Xd </span>
+        <span className="">Certified</span>
         <img src={iconCat} alt=" " className="image__icon" onClick={onClick} />
       </div>
       <div
