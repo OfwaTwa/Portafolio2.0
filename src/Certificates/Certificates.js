4dde42199c2dfa32f06c85ae14fb21b241982d0e
Ofwa Twa <ofwatw@gmail.com>
2023-05-04 03:08:44 -0500

    Portfolio_3.3

--- src/Certificates/Certificates.js
+++ src/Certificates/Certificates.js
@@ -3,6 +3,9 @@ import image2 from "../archivos/Certificates/certificate2.jpg";
 import image3 from "../archivos/Certificates/certificate3.jpg";
 import image4 from "../archivos/Certificates/certificate4.jpg";
 import image5 from "../archivos/Certificates/certificate5.jpg";
+import image5 from "../archivos/Certificates/certificate6.jpg";
+import image5 from "../archivos/Certificates/certificate7.jpg";
+import image5 from "../archivos/Certificates/certificate8.jpg";
 
 export const certificate = [
     image1,
